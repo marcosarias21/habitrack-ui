8ef03579c3666e8c4a8bcee64f6097f58997b423
marcosarias21 <marcosariasolaiz@gmail.com>
2025-07-08 11:40:11 -0300

    Fixed shadcn and login component completed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,9 +2,9 @@ import { LoginUserForm } from '../../components/auth/LoginUserForm'
 
 const Login = () => {
   return (
-    <>
+    <section className="flex h-dvh items-center justify-center">
       <LoginUserForm />
-    </>
+    </section>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
