1748cfceee6a78efb5ff3e6336af4a77a3ef6449
marcosarias21 <marcosariasolaiz@gmail.com>
2025-07-08 15:14:55 -0300

    Sidebar structured created

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
+import { LogOut } from 'lucide-react'
 const Sidebar = () => {
-  return <div></div>
+  return (
+    <div className="flex w-50 flex-col items-center justify-between border-r-1">
+      <div className="mt-5">
+        <h3 className="text-2xl font-bold tracking-wider">
+          <span className="text-blue-400">Habit</span> Track
+        </h3>
+      </div>
+      <div className="flex flex-col gap-5">
+        <Link to={''}>All habits</Link>
+        <Link to={''}>Statistic</Link>
+      </div>
+      <div className="mb-5">
+        <Button variant={'ghost'}>
+          <LogOut color="black" /> Sign Out
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,11 @@
+import { Sidebar } from '@/components/Sidebar'
+
 const Dashboard = () => {
-  return <div></div>
+  return (
+    <section className="flex h-dvh">
+      <Sidebar />
+    </section>
+  )
 }
 
 export default Dashboard
