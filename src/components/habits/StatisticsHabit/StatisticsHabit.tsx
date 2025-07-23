a3ea9475315e1c9ee2afd7692939a063b766e13d
marcosarias21 <marcosariasolaiz@gmail.com>
2025-07-23 12:38:23 -0300

    Added circular progressbar w/percentage progress on day

--- src/components/habits/StatisticsHabit/StatisticsHabit.tsx
+++ src/components/habits/StatisticsHabit/StatisticsHabit.tsx
@@ -1,12 +1,21 @@
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
 type Prop = { percentageDone: number }
 
 const StatisticsHabit: React.FC<Prop> = ({ percentageDone }) => {
   return (
     <div className="mt-10 rounded bg-[#f7f9fb] p-5">
-      <h2 className="text-center text-xl font-bold">Statistics</h2>
+      <h2 className="text-center text-2xl font-bold text-gray-800">
+        Statistics
+      </h2>
       <div className="mt-5 flex flex-col items-center justify-center gap-1">
-        <p className="text-lg font-bold text-blue-600">{percentageDone}%</p>
-        <p className="w-30 text-center text-sm font-medium text-gray-500">
+        <CircularProgressbar
+          className="h-25 w-25"
+          value={percentageDone}
+          text={`${Math.round(percentageDone)}%`}
+        />
+        <p className="w-30 text-center text-sm font-medium text-gray-400">
           Current Day's Progress
         </p>
       </div>
