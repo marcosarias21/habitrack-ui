6a3690f28b73aae26f5e2b8d1b0e0e4007035726
marcosarias21 <marcosariasolaiz@gmail.com>
2025-07-24 10:47:34 -0300

    Custom hook to manage habits and their functions and clean code

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -7,45 +7,18 @@ import { Header } from '@/components/Header'
 import { Sidebar } from '@/components/Sidebar'
 import useDataDay from '@/hooks/useDataDay'
 import useGetMyUser from '@/hooks/useGetMyUser'
-import type { Habit } from '@/interfaces/habit/Habit'
-import {
-  completeHabit,
-  useCreateHabit,
-  useGetHabit,
-} from '@/services/habit/useHabit'
-import { useHabitStore } from '@/store/habitStore'
-import { useEffect, useState } from 'react'
+import useHabitLogic from '@/hooks/useHabitLogic'
 
 const Dashboard = () => {
   const { user } = useGetMyUser()
-  const { days } = useHabitStore()
   const { fullDate, dayIndex, dayName, setDay, day } = useDataDay()
-  const [percentageDone, setPercentageDone] = useState<number>(0)
-  const [habitsData, setHabitsData] = useState<Habit[]>([])
-  const [habitsCompleted, setHabitsCompleted] = useState<Habit[]>([])
-
-  const getHabit = async () => {
-    const { habitsNotDone, allHabitsToday, habitsCompleted } =
-      await useGetHabit(user?._id, dayIndex, fullDate)
-    const doneCount = allHabitsToday - habitsNotDone.length
-    const percentage = (doneCount / allHabitsToday) * 100
-    setPercentageDone(percentage)
-    setHabitsData(habitsNotDone)
-    setHabitsCompleted(habitsCompleted)
-  }
-
-  const createHabit = async (name: string, frequency: string) => {
-    await useCreateHabit(user?._id, name, frequency, days)
-  }
-
-  const onCompleteHabit = async (id: string, date: string) => {
-    await completeHabit(id, date)
-    getHabit()
-  }
-
-  useEffect(() => {
-    getHabit()
-  }, [user, day])
+  const {
+    createHabit,
+    habitsData,
+    habitsCompleted,
+    onCompleteHabit,
+    percentageDone,
+  } = useHabitLogic(user, dayIndex, fullDate, day)
 
   return (
     <section className="grid h-dvh grid-cols-12 gap-4">
@@ -78,11 +51,13 @@ const Dashboard = () => {
             <NoPendingHabits />
           )}
         </div>
-        <div className="flex flex-col rounded-lg bg-[#fff] p-4">
-          <h2 className="font-bold">Habits Completed</h2>
-          {habitsCompleted.map((habit) => (
-            <HabitCard key={habit._id} {...habit} />
-          ))}
+        <div className="rounded-lg bg-[#fff] p-4">
+          <h2 className="text-lg font-bold">Habits Completed</h2>
+          <div className="mt-4 flex flex-col gap-5 px-10">
+            {habitsCompleted.map((habit) => (
+              <HabitCard key={habit._id} {...habit} />
+            ))}
+          </div>
         </div>
       </section>
       <section className="col-span-3 mr-5 bg-[#fff] px-7">
