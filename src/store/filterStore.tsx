abc082346f0d21c1067b1248eeffb189ff7fceb1
marcosarias21 <marcosariasolaiz@gmail.com>
2025-08-06 09:40:09 -0300

    Replace state to zustand state on filter

--- src/store/filterStore.tsx
+++ src/store/filterStore.tsx
@@ -8,7 +8,7 @@ interface Action {
   setFilter: (arg: string) => void
 }
 
-export const useFilterStore = create<Action | State>((set) => ({
+export const useFilterStore = create<Action & State>((set) => ({
   filter: '',
   setFilter: (arg) =>
     set({
