69b39de56792f78e67d4cba4e6296a32cf0d1e93
marcosarias21 <marcosariasolaiz@gmail.com>
2025-09-15 10:04:49 -0300

    Lower text-gray on empty message habit

--- src/components/habits/EmptyHabitsMessage/EmptyHabitsMessage.tsx
+++ src/components/habits/EmptyHabitsMessage/EmptyHabitsMessage.tsx
@@ -1,7 +1,7 @@
 const EmptyHabitsMessage = () => {
   return (
     <div className="bg-[#f7f9fb] p-4 text-center">
-      <p className="text-sm font-semibold text-gray-500">
+      <p className="text-sm font-semibold text-gray-400">
         Don't waste your time and complete your habits!
       </p>
     </div>
