b1ea285fdcc1cfc23b374716eb7a9b37843dbbdf
marcosarias21 <marcosariasolaiz@gmail.com>
2025-09-17 11:55:14 -0300

    new component to show habits stats

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -2,19 +2,32 @@ import { BackgroundContainer } from '@/components/layout/BackgroundContainer'
 import { Container } from '@/components/layout/Container'
 import { Header } from '@/components/layout/Header'
 import { Sidebar } from '@/components/layout/Sidebar'
+import { StatCard } from '@/components/statistics/StatCard'
 import useGetMyUser from '@/hooks/useGetMyUser'
 import useStatistics from '@/hooks/useStatistics'
+import { Grid, Smile } from 'lucide-react'
 
 const Statistics = () => {
   const { user } = useGetMyUser()
-  const { totalHabits } = useStatistics(user?._id ?? '')
+  const { totalHabits, perfectDaysCount } = useStatistics(user?._id ?? '')
   return (
     <Container>
       <Sidebar />
       <section className="col-span-10 flex flex-col gap-4">
         <Header _id="Statistics" email="Statistics" />
         <BackgroundContainer>
-          <div className="flex"></div>
+          <div className="grid grid-cols-3 gap-4">
+            <StatCard
+              data={totalHabits}
+              title="Total Habits"
+              icon={<Smile className="mb-2 text-red-500" size={19} />}
+            />
+            <StatCard
+              data={perfectDaysCount}
+              title="Perfect Days"
+              icon={<Grid className="mb-2 text-red-500" size={19} />}
+            />
+          </div>
         </BackgroundContainer>
       </section>
     </Container>
